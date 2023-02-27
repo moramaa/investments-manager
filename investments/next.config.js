8316607c6325192273c8223d2307efcc6d9d2566
Mor Amir <imoramira@gmail.com>
2023-02-27 10:06:35 +0200

    4

--- investments/next.config.js
+++ investments/next.config.js
@@ -4,3 +4,31 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // modifyVars: { '@primary-color': '#04f' }, // optional
+  lessVarsFilePath: './src/styles/variables.less', // optional
+  lessVarsFilePathAppendToEndOfContent: false, // optional
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {
+    // ...
+    mode: "local",
+    localIdentName: "[hash:base64:8]",
+    exportLocalsConvention: "camelCase",
+    exportOnlyLocals: false,
+    // ...
+    getLocalIdent: (context, localIdentName, localName, options) => {
+      return "whatever_random_class_name";
+    },
+  },
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+});
